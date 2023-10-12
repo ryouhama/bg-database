e6874035a9aa40ba415c32ab9f3640416cb45fa2
ryouhama <r0730hacap@icloud.com>
2023-10-12 19:53:54 +0900

    コメント追加

--- src/components/Graph/LineChart.tsx
+++ src/components/Graph/LineChart.tsx
@@ -16,6 +16,10 @@ export type DataSet = {
   data: Data[]
 }
 
+// TODO: `xxFrame`みたいな感じになってしまい、本来のComponentからズレてる
+// 理想は、component -> パーツ、features -> 機能
+// 機能寄りの内容が多いため、featuresに移行する。Recharts自体が豊富に機能を提供しているため、ラップしたものは作らない様にしたい
+// ただし、型は作りたし
 export const LineChartFrame: React.FC<PropsWithChildren<Props>> = (props) => {
   const { width, height, dataset, children } = props
 
@@ -26,6 +30,7 @@ export const LineChartFrame: React.FC<PropsWithChildren<Props>> = (props) => {
       width={width || undefined}
       height={height || undefined}
       data={dataWithConverted}
+      // TODO: marginなどのスタイルは外から渡せる様にする
       margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
     >
       <CartesianGrid />
@@ -42,6 +47,7 @@ export const LineChartFrame: React.FC<PropsWithChildren<Props>> = (props) => {
 const convertDatasetToLineChartData = (dataset: DataSet) => {
   const title = dataset.title
   return dataset.data.map((it) => ({
+    // TODO: x軸は入力されたデータを加工せず参照する
     name: `2023-10-${it.x}`,
     [title]: it.y
   }))
